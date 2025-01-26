115de776e625b893c2fb1272f3a83108a9171420
stakancheck <stakancheck@gmail.com>
2025-01-26 20:18:38 +0300

    init: Add linters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.spotless)
 }
 
+// Android configuration ---------------------------------------------------------------------------
 android {
     namespace = "io.github.stakancheck.githubviewer"
     compileSdk = 34
@@ -25,7 +27,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -49,21 +51,62 @@ android {
     }
 }
 
+// Spotless configuration --------------------------------------------------------------------------
+spotless {
+    kotlin {
+        ktfmt("0.51").googleStyle().configure {
+            it.setMaxWidth(80)
+            it.setBlockIndent(4)
+            it.setContinuationIndent(4)
+            it.setRemoveUnusedImports(false)
+            it.setManageTrailingCommas(false)
+        }
+        ktlint("1.0.0")
+            .editorConfigOverride(
+                mapOf(
+                    "indent_size" to 4,
+                    // intellij_idea is the default style we preset in Spotless, you can override it referring to https://pinterest.github.io/ktlint/latest/rules/code-styles.
+                    "ktlint_code_style" to "intellij_idea",
+                ),
+            ).customRuleSets(
+                listOf(
+                    "io.nlopez.compose.rules:ktlint:0.4.16",
+                ),
+            )
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
+}
+
+tasks.register("spotlessApplyAll") {
+    group = "spotless"
+    description = "Apply spotless to all supported files"
+    dependsOn("spotlessApply")
+}
+
+// Dependencies ------------------------------------------------------------------------------------
 dependencies {
 
+    // Android X dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
+    implementation(platform(libs.androidx.compose.bom)) // Compose
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Testing dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Debugging
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
